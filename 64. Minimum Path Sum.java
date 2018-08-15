ce087fd6c3e669bc6faccc3c4c46eec99ff673a4
trytry3 <yrtyrtmai@gmail.com>
2018-08-15 00:26:55 -0700

    add

---
+++ 64. Minimum Path Sum.java
@@ -0,0 +1,35 @@
+/** Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+Note: You can only move either down or right at any point in time.
+
+Example:
+Input:
+[
+  [1,3,1],
+  [1,5,1],
+  [4,2,1]
+]
+Output: 7
+Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+*/
+
+// dynamic programming
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int[][] res = new int[rows][cols];
+        res[0][0] = grid[0][0];
+        for (int j = 1; j < cols; j++) {
+            res[0][j] = grid[0][j] + res[0][j-1];
+        }
+        for (int i = 1; i < rows; i++) {
+            res[i][0] = grid[i][0] + res[i-1][0];
+        }
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                res[i][j] = Math.min(res[i-1][j], res[i][j-1]) + grid[i][j];
+            }
+        }
+        return res[rows-1][cols-1];
+    }
+}
---
+++ 74. Search a 2D Matrix.java
@@ -0,0 +1,44 @@
+/** Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+
+Example:
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 3
+Output: true
+*/
+
+// binary search
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0 || matrix[0].length == 0)
+            return false;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int start = 0, end = rows*cols - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start)/2;
+            int row = mid / cols;
+            int col = mid % cols;
+            if (matrix[row][col] > target) {
+                end = mid;
+            } else if (matrix[row][col] == target) {
+                return true;
+            } else {
+                start = mid;
+            }
+        }
+        if (matrix[start/cols][start%cols] == target) {
+            return true;
+        }
+        if (matrix[end/cols][end%cols] == target) {
+            return true;
+        }
+        return false;       
+    }
+}
