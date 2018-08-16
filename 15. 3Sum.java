a3e074d94e3fd286e358fd5df8b6d347533ce43f
trytry3 <yrtyrtmai@gmail.com>
2018-08-16 12:45:34 -0700

    add

---
+++ 15. 3Sum.java
@@ -0,0 +1,48 @@
+/** Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? 
+Find all unique triplets in the array which gives the sum of zero.
+Note:
+The solution set must not contain duplicate triplets.
+
+Example:
+Given array nums = [-1, 0, 1, 2, -1, -4],
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+*/
+
+/** thinking: 
+Sort an input array and then run through all indices of a possible first element of a triplet. 
+For each possible first element we make a standard bi-directional 2Sum sweep of the remaining part of the array. 
+*/
+// two pointers
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i + 2 < nums.length; i++) {
+            // skip same element, since nums[i-1] is already tested as target
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int j = i + 1, k = nums.length - 1, target = -nums[i];
+            while (j < k) {
+                if (nums[j] + nums[k] == target) {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    // skip same element, since nums[j-1] is already tested
+                    while (j < k && nums[j] == nums[j-1])
+                        j++;
+                    while (j < k && nums[k] == nums[k+1])
+                        k--;
+                } else if (nums[j] + nums[k] > target) {
+                    k--;
+                } else if (nums[j] + nums[k] < target) {
+                    j++;
+                }  
+            }
+        }
+        return res;      
+    }
+}
---
+++ 79. Word Search.java
@@ -0,0 +1,48 @@
+/** Given a 2D board and a word, find if the word exists in the grid.
+The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. 
+The same letter cell may not be used more than once.
+
+Example:
+board =
+[
+  ['A','B','C','E'],
+  ['S','F','C','S'],
+  ['A','D','E','E']
+]
+Given word = "ABCCED", return true.
+Given word = "SEE", return true.
+Given word = "ABCB", return false.
+*/
+
+// dfs, backtracking
+class Solution {
+    private boolean[][] visited;
+    
+    public boolean exist(char[][] board, String word) {
+        visited = new boolean[board.length][board[0].length];    
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                if((word.charAt(0) == board[i][j]) && dfs(board, word, i, j, 0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    private boolean dfs(char[][]board, String word, int i, int j, int indexInWord){
+        if (indexInWord == word.length())
+            return true; // because nothing needs to match
+        if (i >= board.length || i < 0 || j >= board[i].length || j < 0 || 
+            board[i][j] != word.charAt(indexInWord) || visited[i][j])
+                return false;
+        // here board[i][j] == word.charAt(indexInWord), search if next char matches
+        visited[i][j] = true;
+        if (dfs(board, word, i-1, j, indexInWord+1) || dfs(board, word, i+1, j, indexInWord+1) ||
+            dfs(board, word, i, j-1, indexInWord+1) || dfs(board, word, i, j+1, indexInWord+1))
+                return true;
+        // next char can't find matched path, backtracking
+        visited[i][j] = false;
+        return false;
+    }
+}
