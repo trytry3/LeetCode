fd2ec6856a861f58e772bc4981003f7ec031ec5a
trytry3 <yrtyrtmai@gmail.com>
2018-08-21 00:25:43 -0700

    add

---
+++ 125. Valid Palindrome.java
@@ -0,0 +1,28 @@
+/* Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+Input: "A man, a plan, a canal: Panama"
+Output: true
+
+Example 2:
+Input: "race a car"
+Output: false
+*/
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        int start = 0, end = s.length()-1;
+        while (start < end) {
+            while (start < end && !Character.isLetterOrDigit(s.charAt(start)))
+                start++;
+            while (end > start && !Character.isLetterOrDigit(s.charAt(end)))
+                end--;
+            if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) 
+                return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
---
+++ 98. Validate Binary Search Tree.java
@@ -0,0 +1,71 @@
+/* Given a binary tree, determine if it is a valid binary search tree (BST).
+Assume a BST is defined as follows:
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+
+Example 1:
+Input:
+    2
+   / \
+  1   3
+Output: true
+
+Example 2:
+
+    5
+   / \
+  1   4
+     / \
+    3   6
+Output: false
+Explanation: The input is: [5,1,4,null,null,3,6]. The root node's value
+             is 5 but its right child's value is 4.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+// method 1: dfs
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    private boolean isValidBST(TreeNode root, long min, long max) {
+        if (root == null)
+            return true;
+        if (root.val <= min || root.val >= max)
+            return false;
+        return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+    }
+}
+
+// method 2: see iterative method of 94. Binary Tree Inorder Traversal.java
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) 
+            return true;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pre = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if(pre != null && root.val <= pre.val) 
+                return false;
+            pre = root;
+            root = root.right;
+        }
+        return true;
+    }
+}
