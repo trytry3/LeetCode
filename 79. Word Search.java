e732d9fb2afaaa7875b05f2ef83c050a33940f42
trytry3 <yrtyrtmai@gmail.com>
2018-09-05 12:29:39 -0700

    add

--- 79. Word Search.java
+++ 79. Word Search.java
@@ -20,9 +20,9 @@ class Solution {
     
     public boolean exist(char[][] board, String word) {
         visited = new boolean[board.length][board[0].length];    
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[i].length; j++){
-                if((word.charAt(0) == board[i][j]) && dfs(board, word, i, j, 0)){
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if ((word.charAt(0) == board[i][j]) && dfs(board, word, i, j, 0)) {
                     return true;
                 }
             }
@@ -30,7 +30,7 @@ class Solution {
         return false;
     }
     
-    private boolean dfs(char[][]board, String word, int i, int j, int indexInWord){
+    private boolean dfs(char[][]board, String word, int i, int j, int indexInWord) {
         if (indexInWord == word.length())
             return true; // because nothing needs to match
         if (i >= board.length || i < 0 || j >= board[i].length || j < 0 || 
