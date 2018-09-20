d13fec88cea9d9104720b98b45580cc4cae857af
trytry3 <yrtyrtmai@gmail.com>
2018-09-19 23:37:48 -0700

    add

--- 336. Palindrome Pairs.java
+++ 336. Palindrome Pairs.java
@@ -52,7 +52,7 @@ class Solution {
     
     private boolean isPalindrome(String word) {
         int i = 0, j = word.length() - 1;
-        while(i < j) {
+        while (i < j) {
             if (word.charAt(i) != word.charAt(j))
                 return false;
             i++;
---
+++ 680. Valid Palindrome II.java
@@ -0,0 +1,42 @@
+/* Given a non-empty string s, you may delete at most one character. 
+Judge whether you can make it a palindrome.
+
+Example 1:
+Input: "aba"
+Output: True
+
+Example 2:
+Input: "abca"
+Output: True
+Explanation: You could delete the character 'c'.
+
+Note:
+The string will only contain lowercase characters a-z. The maximum length of the string is 50000.
+*/
+
+class Solution {
+    public boolean validPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j && s.charAt(i) == s.charAt(j)) {
+            i++; 
+            j--;
+        }
+
+        if (i >= j) 
+            return true;
+
+        if (isPalinedrome(s, i + 1, j) || isPalinedrome(s, i, j - 1)) 
+            return true;
+        return false;
+    }
+
+    private boolean isPalinedrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) 
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
