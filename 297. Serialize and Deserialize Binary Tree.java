449f0b1f3d3e04d1ef292422334426f0a15bee46
trytry3 <yrtyrtmai@gmail.com>
2018-09-20 00:12:00 -0700

    add

---
+++ 297. Serialize and Deserialize Binary Tree.java
@@ -0,0 +1,79 @@
+/* Serialization is the process of converting a data structure or object into a sequence of bits 
+so that it can be stored in a file or memory buffer, or transmitted across a network connection link 
+to be reconstructed later in the same or another computer environment.
+
+Design an algorithm to serialize and deserialize a binary tree. 
+There is no restriction on how your serialization/deserialization algorithm should work. 
+You just need to ensure that a binary tree can be serialized to a string 
+and this string can be deserialized to the original tree structure.
+
+Example: 
+You may serialize the following tree:
+
+    1
+   / \
+  2   3
+     / \
+    4   5
+
+as "[1,2,3,null,null,4,5]"
+
+Clarification: The above format is the same as how LeetCode serializes a binary tree. 
+You do not necessarily need to follow this format, so please be creative and come up 
+with different approaches yourself.
+
+Note: Do not use class member/global/static variables to store states. 
+Your serialize and deserialize algorithms should be stateless.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        return serializeHelper(root, "");
+    }
+    
+    private String serializeHelper(TreeNode root, String str) {
+        if (root == null) {
+            str += "null,";
+        } else {
+            str += str.valueOf(root.val) + ",";
+            str = serializeHelper(root.left, str);
+            str = serializeHelper(root.right, str);
+        }
+        return str;
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String[] dataArray = data.split(",");
+        // to make it not fixed size
+        List<String> list = new ArrayList<>(Arrays.asList(dataArray));
+        return deserializeHelper(list);
+    }
+    
+    private TreeNode deserializeHelper(List<String> list) {
+        if (list.get(0).equals("null")) {
+            list.remove(0);
+            return null;
+        }
+        TreeNode root = new TreeNode(Integer.valueOf(list.get(0)));
+        list.remove(0);
+        root.left = deserializeHelper(list);
+        root.right = deserializeHelper(list);
+        return root;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.deserialize(codec.serialize(root));
