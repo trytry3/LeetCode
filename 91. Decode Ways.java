081fec142188d06ba5ae738f53981a45e8bf1cfb
trytry3 <yrtyrtmai@gmail.com>
2018-09-19 23:27:32 -0700

    add

--- 91. Decode Ways.java
+++ 91. Decode Ways.java
@@ -18,6 +18,7 @@ Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
 
 // dynamic programming
 class Solution {
+    // may contain '0', which is not decoded
     public int numDecodings(String s) {
         if(s == null || s.length() == 0) {
             return 0;
@@ -27,9 +28,7 @@ class Solution {
         int[] dp = new int[n+1];
         // dp[0] means an empty string has one way to decode
         dp[0] = 1;
-        // 0 is not in the mapping
         dp[1] = s.charAt(0) == '0' ? 0 : 1;
-
         for (int i = 2; i <= n; i++) {
             int oneDigit = Integer.valueOf(s.substring(i-1, i));
             int twoDigit = Integer.valueOf(s.substring(i-2, i));
@@ -37,7 +36,7 @@ class Solution {
                dp[i] = dp[i-1];  
             }
             if (twoDigit >= 10 && twoDigit <= 26) {
-                dp[i] += dp[i-2];
+                dp[i] += dp[i-2]; // because need to add dp[i-1] if possible
             }
         }
         return dp[n];
