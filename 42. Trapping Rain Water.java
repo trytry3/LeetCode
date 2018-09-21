fff0c827df8cc61eb12fc0f13e633ddb3f78cf78
trytry3 <yrtyrtmai@gmail.com>
2018-09-20 20:39:38 -0700

    add

--- 42. Trapping Rain Water.java
+++ 42. Trapping Rain Water.java
@@ -6,7 +6,31 @@ Input: [0,1,0,2,1,0,1,3,2,1,2,1]
 Output: 6
 */
 
-// two pointers
+// method 1:
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int sum = 0;
+        if (n == 0)
+            return sum;
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+        leftMax[0] = height[0];
+        rightMax[n-1] = height[n-1];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i-1], height[i]); 
+        }
+        for (int i = n-2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i+1], height[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            sum += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return sum;
+    }
+}
+
+// method 2: two pointers
 class Solution {
     public int trap(int[] height) {
         int left = 0, right = height.length-1;
