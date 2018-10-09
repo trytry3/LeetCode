3b3090e62fdada86d454f1db7773284320357017
trytry3 <yrtyrtmai@gmail.com>
2018-10-08 23:14:03 -0700

    add

--- Wizards Min Cost.java
+++ Wizards Min Cost.java
@@ -1,3 +1,10 @@
+/* There are 10 wizards, 0-9, you are given a list that each entry is a list of wizards known by wizard. 
+Define the cost between wizards and wizard as square of different of i and j. To find the min cost between 0 and 9.
+
+Example:
+wizard[0]: 1, 4, 5; wizard[4]: 9 
+wizard 0 to 9 min distance is (0-4)^2+(4-9)^2=41 (wizard[0]->wizard[4]->wizard[9])
+*/
 
 // dijkstra
 // airbnb
