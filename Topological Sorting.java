36dc249540f09f94319c039ab01d9ede50fd7602
trytry3 <42289067+trytry3@users.noreply.github.com>
2020-03-15 16:29:03 -0700

    Update Topological Sorting.java

--- Topological Sorting.java
+++ Topological Sorting.java
@@ -1,5 +1,5 @@
 /*
-Given an directed graph, a topological order of the graph nodes is defined as follow:
+Given a directed graph, a topological order of the graph nodes is defined as follows:
 - For each directed edge A -> B in graph, A must before B in the order list.
 - The first node in the order can be any node in the graph with no nodes direct to it.
 Find any topological order for the given graph.
