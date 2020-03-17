7f6359670bf77a3d47d787e82174e4f5e053e106
trytry3 <42289067+trytry3@users.noreply.github.com>
2020-03-17 14:04:02 -0700

    Update 207. Course Schedule.java

--- 207. Course Schedule.java
+++ 207. Course Schedule.java
@@ -14,6 +14,10 @@ Output: false
 Explanation: There are a total of 2 courses to take. 
              To take course 1 you should have finished course 0, and to take course 0 you should
              also have finished course 1. So it is impossible.
+
+Example 3:
+Input: 1, [] 
+Output: true
 */
 
 // topological sorting
