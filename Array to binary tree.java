04e66d4e0cec75f4ebbad4fd13881e3fcd2eb0d4
trytry3 <42289067+trytry3@users.noreply.github.com>
2020-04-11 16:30:04 -0700

    Update Array to binary tree.java

--- Array to binary tree.java
+++ Array to binary tree.java
@@ -1,3 +1,82 @@
+/* Construct binary tree from array, print the tree
+*/
+class Solution {
+
+	public static <T> Node<T> fromArray(T[] array) {
+		return fromArrayHelper(array, 0, array.length - 1);
+
+	}
+
+	// O(n), avoid copying array
+	private static <T> Node<T> fromArrayHelper(T[] array, int start, int end) {
+		if (start > end)
+			return null;
+		// find mid point
+		int midIndex = (start + end) / 2;
+		Node<T> leftNode = fromArrayHelper(array, start, midIndex - 1);
+		Node<T> rightNode = fromArrayHelper(array, midIndex + 1, end);
+		Node<T> midNode = new Node<>(array[midIndex], leftNode, rightNode);
+		return midNode;
+	}
+
+	public static void main(String[] args) {
+		Node<Integer> root = new Node<Integer>(0);
+		System.out.println(root);
+		// "(0)"
+
+		Node<Integer> root2 = new Node<Integer>(1, new Node<Integer>(0), new Node<Integer>(2));
+		System.out.println(root2);
+		// "((0)1(2))"
+
+		Integer[] v = { 1, 2, 3, 4, 5, 6, 7, };
+		System.out.println(fromArray(v));
+		// (((1)2(3))4((5)6(7)))
+	}
+}
+
+class Node<T> {
+	T item;
+	Node<T> left;
+	Node<T> right;
+
+	public Node(T item) {
+		this.item = item;
+	}
+
+	public Node(T item, Node<T> left, Node<T> right) {
+		this.item = item;
+		this.left = left;
+		this.right = right;
+	}
+
+	@Override
+	// more efficient
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		helper(sb, this);
+		return sb.toString();
+	}
+
+	private void helper(StringBuilder sb, Node<T> curNode) {
+		if (curNode == null)
+			return;
+		sb.append("(");
+		helper(sb, curNode.left);
+		sb.append(curNode.item.toString());
+		helper(sb, curNode.right);
+		sb.append(")");
+	}
+
+	public String toString2() {
+		String leftStr = left == null ? "" : left.toString();
+		String rightStr = right == null ? "" : right.toString();
+		return "(" + leftStr + item.toString() + rightStr + ")";
+	}
+
+}
+
+
+
 /* Construct binary tree from a given array in level order fashion 
 */
 
