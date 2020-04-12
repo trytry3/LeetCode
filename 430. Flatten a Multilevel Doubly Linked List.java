de1c6122575fc6b1ca7f6888bdc6d88ca0116db8
trytry3 <42289067+trytry3@users.noreply.github.com>
2020-04-12 16:10:10 -0700

    Update 430. Flatten a Multilevel Doubly Linked List.java

--- 430. Flatten a Multilevel Doubly Linked List.java
+++ 430. Flatten a Multilevel Doubly Linked List.java
@@ -36,29 +36,31 @@ class Node {
 };
 */
 class Solution {
-	public Node flatten(Node head) {
-		if (head == null)
-			return head;
-		Node p = head;
-		while (p != null) {
-			// if no child, proceed
-			if (p.child == null) {
-				p = p.next;
-				continue;
-			}
-			// got child, find the tail of the child and link it to p.next
-			Node temp = p.child;
-			while (temp.next != null)
-				temp = temp.next;
+    public Node flatten(Node head) {
+        if (head == null)
+            return head;
+        Node p = head;
+        while (p != null) {
+            // if no child, proceed
+            if (p.child == null) {
+                p = p.next;
+                continue;
+            }
+            
+            // got child, find the tail of the child and link it to p.next
+            Node tmp = p.child;
+            while (tmp.next != null)
+                tmp = tmp.next;
             // connect child's tail and p.next
-			temp.next = p.next;
-			if (p.next != null)
-				p.next.prev = temp;
-			// connect p with p.child, and remove p.child
-			p.next = p.child;
-			p.child.prev = p;
-			p.child = null;
-		}
-		return head;
-	}
+            tmp.next = p.next;
+            if (p.next != null)
+                p.next.prev = tmp;
+            
+            // connect p with p.child, and remove p.child
+            p.next = p.child;
+            p.child.prev = p;
+            p.child = null;
+        }
+        return head;
+    }
 }
