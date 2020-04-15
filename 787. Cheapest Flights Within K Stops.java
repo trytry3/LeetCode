073b98740d50397003ee383e3a27a9b5b18ae424
trytry3 <42289067+trytry3@users.noreply.github.com>
2020-04-14 20:14:42 -0700

    Update 787. Cheapest Flights Within K Stops.java

--- 787. Cheapest Flights Within K Stops.java
+++ 787. Cheapest Flights Within K Stops.java
@@ -1,5 +1,5 @@
-/* There are n cities connected by m flights. Each fight starts from city u and arrives at v with a price w.
-Now given all the cities and fights, together with starting city src and the destination dst, 
+/* There are n cities connected by m flights. Each flight starts from city u and arrives at v with a price w.
+Now given all the cities and flights, together with starting city src and the destination dst, 
 your task is to find the cheapest price from src to dst with up to k stops. If there is no such route, output -1.
 
 Example 1:
