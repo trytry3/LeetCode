7a000b827f6ab990c8f48d1125d84b288fe0124f
trytry3 <42289067+trytry3@users.noreply.github.com>
2023-07-23 18:04:00 -0700

    Update 224. Basic Calculator.java

--- 224. Basic Calculator.java
+++ 224. Basic Calculator.java
@@ -21,29 +21,30 @@ Do not use the eval built-in library function.
 
 class Solution {
     public static int calculate(String s) {
-	    int len = s.length(), sign = 1, result = 0;
-	    Stack<Integer> stack = new Stack<Integer>();
-	    for (int i = 0; i < len; i++) {
-		    if (Character.isDigit(s.charAt(i))) {
-			    int sum = s.charAt(i) - '0';
-			    while (i + 1 < len && Character.isDigit(s.charAt(i + 1))) {
-				    sum = sum * 10 + s.charAt(i + 1) - '0';
-				    i++;
-			    }
-			    result += sum * sign;
-		    } else if (s.charAt(i) == '+')
-			    sign = 1;
-		    else if (s.charAt(i) == '-')
-			    sign = -1;
-		    else if (s.charAt(i) == '(') {
-			    stack.push(result);
-			    stack.push(sign);
-			    result = 0;
-			    sign = 1;
-		    } else if (s.charAt(i) == ')') {
-			    result = result * stack.pop() + stack.pop();
-		    }
-	    }
-	    return result;
+        int len = s.length(), sign = 1, result = 0;
+        Stack<Integer> stack = new Stack<>(); // store numbers and signs
+        for (int i = 0; i < len; i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                int sum = c - '0';
+                while (i + 1 < len && Character.isDigit(s.charAt(i + 1))) {
+                    sum = sum * 10 + s.charAt(i + 1) - '0';
+                    i++;
+                }
+                result += sum * sign;
+            } else if (c == '+')
+                sign = 1;
+            else if (c == '-')
+                sign = -1;
+            else if (c == '(') {
+                stack.push(result);
+                stack.push(sign);
+                result = 0;
+                sign = 1;
+            } else if (c == ')') {
+                result = result * stack.pop() + stack.pop();
+            }
+        }
+        return result;
     }
 }
