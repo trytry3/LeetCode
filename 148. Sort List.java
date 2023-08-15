4aeb9856e058517ad6e34166dde36a456f4902e9
trytry3 <42289067+trytry3@users.noreply.github.com>
2023-08-15 16:56:08 -0700

    Update 148. Sort List.java

--- 148. Sort List.java
+++ 148. Sort List.java
@@ -19,44 +19,44 @@ Output: -1->0->3->4->5
  */
 class Solution {
 
-	public ListNode sortList(ListNode head) {
-		if (head == null || head.next == null)
-			return head;
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
 
-		// cut the list to two halves
-		ListNode prev = null, slow = head, fast = head;
-		while (fast != null && fast.next != null) {
-			prev = slow;
-			slow = slow.next;
-			fast = fast.next.next;
-		}
-		prev.next = null;
+        // cut the list to two halves
+        ListNode prev = null, slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
 
-		// sort each half
-		ListNode l1 = sortList(head);
-		ListNode l2 = sortList(slow);
+        // sort each half
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(slow);
 
-		// merge l1 and l2
-		return merge(l1, l2);
-	}
+        // merge l1 and l2
+        return merge(l1, l2);
+    }
 
-	private ListNode merge(ListNode l1, ListNode l2) {
-		ListNode dummy = new ListNode(0);
-        ListNode cur = dummy;   
-		while (l1 != null && l2 != null) {
-			if (l1.val < l2.val) {
-				cur.next = l1;
-				l1 = l1.next;
-			} else {
-				cur.next = l2;
-				l2 = l2.next;
-			}
-			cur = cur.next;
-		}
-		if (l1 != null)
-			cur.next = l1;
-		if (l2 != null)
-			cur.next = l2;
-		return dummy.next;
-	}
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        if (l1 != null)
+            cur.next = l1;
+        if (l2 != null)
+            cur.next = l2;
+        return dummy.next;
+    }
 }
