d2fabc33936a0da398deb49db4b626beaa7d8d02
trytry3 <42289067+trytry3@users.noreply.github.com>
2023-08-15 20:46:35 -0700

    Update 200. Number of Islands.java

--- 200. Number of Islands.java
+++ 200. Number of Islands.java
@@ -15,32 +15,32 @@ Output: 3
 
 class Solution {
     public int numIslands(char[][] grid) {
-		int count = 0;
-		int rows = grid.length;
-		if (rows == 0) 
-			return 0;
-		int cols = grid[0].length;
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < cols; j++) {
-				if (grid[i][j] == '1') {
-					count++;
-					dfsMarking(grid, i, j);
-				}
-			}
-		}
-		return count;
-	}
+        int count = 0;
+        int rows = grid.length;
+        if (rows == 0)
+            return 0;
+        int cols = grid[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfsMarking(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
 
-	private void dfsMarking(char[][] grid, int i, int j) {
-		if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) {
-			return;
-		}
-		if (grid[i][j] == '1') {
-			grid[i][j] = '0';
-			dfsMarking(grid, i - 1, j);
-			dfsMarking(grid, i + 1, j);
-			dfsMarking(grid, i, j - 1);
-			dfsMarking(grid, i, j + 1);
-		}
-	}
+    private void dfsMarking(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) {
+            return;
+        }
+        if (grid[i][j] == '1') {
+            grid[i][j] = '0';
+            dfsMarking(grid, i - 1, j);
+            dfsMarking(grid, i + 1, j);
+            dfsMarking(grid, i, j - 1);
+            dfsMarking(grid, i, j + 1);
+        }
+    }
 }
