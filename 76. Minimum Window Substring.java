faa91c48f3ec942a717b8a0e5a42853304876413
trytry3 <42289067+trytry3@users.noreply.github.com>
2023-08-16 22:18:19 -0700

    Update 76. Minimum Window Substring.java

--- 76. Minimum Window Substring.java
+++ 76. Minimum Window Substring.java
@@ -12,25 +12,25 @@ If there is such window, you are guaranteed that there will always be only one u
 // two pointers
 // template for LeetCode substring search problem
 class Solution {
-	public String minWindow(String str, String pat) {
+    public String minWindow(String str, String pat) {
         if (str.length() < pat.length())
             return "";
         int minWindowLen = Integer.MAX_VALUE;
         // store leftover char frequency when matching `pat` in `str`
         Map<Character, Integer> map = new HashMap<>();
-        for (char c: pat.toCharArray()) {
-            map.put(c, map.getOrDefault(c,0)+1);
+        for (char c : pat.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
         // store unmatched char counts
         int unMatchedCharCount = map.size();
         // two pointers marking the matching interval
         int start = 0, end = 0;
-        // store the min matched window starting position 
+        // store the min matched window starting position
         int head = 0;
         while (end < str.length()) {
             char e = str.charAt(end);
             if (map.containsKey(e)) {
-                map.put(e, map.get(e)-1);
+                map.put(e, map.get(e) - 1);
                 if (map.get(e) == 0)
                     unMatchedCharCount--;
             }
@@ -43,14 +43,14 @@ class Solution {
                 }
                 char s = str.charAt(start);
                 if (map.containsKey(s)) {
-                    map.put(s, map.get(s)+1);
+                    map.put(s, map.get(s) + 1);
                     // due to duplicate chars, map can have negative value
                     if (map.get(s) > 0)
                         unMatchedCharCount++;
-                } 
+                }
                 start++;
             }
         }
-        return minWindowLen == Integer.MAX_VALUE ? "" : str.substring(head, head+minWindowLen);
-	}
+        return minWindowLen == Integer.MAX_VALUE ? "" : str.substring(head, head + minWindowLen);
+    }
 }
